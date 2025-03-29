b764c1adfbca4b9700326a4449b662f7e0f4fd71
G30-E <gfloriane@miumg.edu.gt>
2025-03-28 23:13:03 -0600

    Programa Subido

---
+++ Validaciones/p1bpoo/Clases/Moto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1bpoo.MisClases
+{
+    public class Moto : Vehiculo
+    {
+        public Moto(int anio, string elColor, string elModelo) : base(anio, elColor, elModelo)
+        {
+            tiposLicenciaAceptados = new List<string> { "M" };
+        }
+
+
+        protected override int VelocidadMaxima { get; set; } = 120;
+        protected override int CapacidadTanque { get; set; } = 10;
+        protected override double ConsumoCombustible { get; set; } = 3.5;
+
+        public void HacerCaballito()
+        {
+            if (velocidad > 95)
+            {
+                Console.WriteLine("La Moto esta haciendo golpe de gas");
+            }
+            else
+            {
+                Console.WriteLine("No se puede hacer golpe de gas si no ha llega al kilometraje necesario de 80 km/h");
+            }
+        }
+
+        public override void acelerar(int cuanto)
+        {
+            if (velocidad + cuanto <= VelocidadMaxima)
+            {
+                velocidad += cuanto;
+                Console.WriteLine("La Moto ha acelerado a {0} km/h.", velocidad);
+            }
+            else
+            {
+                Console.WriteLine("No se puede exceder la velocidad máxima de {0} km/h.", VelocidadMaxima);
+            }
+        }
+
+        public override void Frenar(int cuanto)
+        {
+            if (velocidad - cuanto >= 0)
+            {
+                velocidad -= cuanto;
+                Console.WriteLine("La moto ha frenado a {0} km/h.", velocidad);
+            }
+            else
+            {
+                Console.WriteLine("No se puede frenar más, la moto se está detenido.");
+            }
+        }
+        public override string TipoLicencia()
+        {
+            return "Licencia tipo M requerida para conducir esta moto.";
+
+        }
+    }   
+        
+}
---
+++ Validaciones/p1bpoo/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using p1bpoo.MisClases;
+
+
+//pilotos:
+
+Chofer pilot1 = new Chofer("Carter", 30, "A");
+Chofer pilot2 = new Chofer("Kanye", 35, "B");
+Chofer pilot3 = new Chofer("Tyler", 28, "C");
+Chofer pilot4 = new Chofer("Ken Carson", 25, "M");
+Chofer pilot5 = new Chofer("Jonatan", 29, "C");
+
+Console.WriteLine("Moto: ");
+Moto suzuki = new Moto(2025, "Gris", "Suzuki-Ns 125");
+suzuki.InformacionVehiculo();
+Console.WriteLine(" ");
+
+Console.WriteLine(suzuki.AsignarPiloto(pilot4));
+pilot4.MostrarInformación();
+pilot4.MostrarValidacion();
+Console.WriteLine(" ");
+
+suzuki.Encender();
+suzuki.acelerar(100);
+suzuki.HacerCaballito();
+suzuki.Frenar(20);
+suzuki.acelerar(130);
+suzuki.Frenar(50);
+suzuki.Frenar(20);
+suzuki.HacerCaballito();
+suzuki.Apagar();
+
+Console.WriteLine("----------------------------------------------------------------------\n");
+Console.WriteLine("----------------------------------------------------------------------\n");
+
+Console.WriteLine("Taxi: ");
+Taxi retinto = new Taxi(2012, "Blanco", "Toyota");
+retinto.InformacionVehiculo();
+retinto.verCantidadPasajeros();
+Console.WriteLine(" ");
+
+Console.WriteLine(retinto.AsignarPiloto(pilot5));
+pilot5.MostrarInformación();
+pilot5.MostrarValidacion();
+Console.WriteLine(" ");
+Console.WriteLine(retinto.AsignarPiloto(pilot2));
+pilot2.MostrarInformación();
+pilot2.MostrarValidacion();
+Console.WriteLine(" ");
+
+retinto.Encender();
+retinto.acelerar(100);
+retinto.Frenar(20);
+retinto.Apagar();
+retinto.acelerar(180);
+retinto.Frenar(95);
+retinto.Apagar();
+
+
+Console.WriteLine("----------------------------------------------------------------------\n");
+Console.WriteLine("----------------------------------------------------------------------\n");
+Console.WriteLine("Autobus:");
+autoBus bus = new autoBus(2010, "Verde y Blanco", "Toyota");
+bus.InformacionVehiculo();
+bus.VerCantidadAsientos();
+Console.WriteLine(" ");
+
+Console.WriteLine(bus.AsignarPiloto(pilot1));
+pilot1.MostrarInformación();
+pilot1.MostrarValidacion();
+Console.WriteLine(" ");
+
+bus.Encender();
+bus.acelerar(50);
+bus.Frenar(20);
+bus.acelerar(110);
+bus.Frenar(50);
+bus.Apagar();
+
+
+
+
+
+
+
+
+
